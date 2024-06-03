2d685a8bf1aa57e26acce35f3dcc21d29c17dce4
Shiwani Katiyar <shiwanikatiyarknj@gmail.com>
2024-06-03 15:04:11 +0530

    add

--- E-Commerce/src/Components/Navbar.jsx
+++ E-Commerce/src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {AuthContext} from '../Context/AuthContextProvider'
+import { useContext } from 'react'
 
 
 const links = [
@@ -17,8 +18,7 @@ const links = [
 
 
 function Navbar() {
-
-
+    const {logout} = useContext(AuthContext)
   return (
     <Container maxW={'100%'} border={'1px solid'} background={'lightpink'}style={{height: "60px"}}>
         <Flex justifyContent={'space-around'} py={'1em'}>
@@ -27,7 +27,7 @@ function Navbar() {
                 <Link to={link.to} key ={link.to}>{link.label}</Link>
             ))
         }
-           <Button colorScheme='pink' variant={'outline'}> LOGOUT</Button>
+           <Button colorScheme='pink' variant={'outline'}onClick={logout}> LOGOUT</Button>
         </Flex>
     </Container>
   )
